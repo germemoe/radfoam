826bff11db6969e3935d9a603ceccaf8fe890335
germemoe <947914628@qq.com>
2025-05-02 16:06:31 +0800

    add alpha channel in colmap

--- data_loader/colmap.py
+++ data_loader/colmap.py
@@ -28,6 +28,8 @@ class COLMAPDataset:
         self.colmap_dir = os.path.join(datadir, "sparse/0/")
         self.split = split
         self.downsample = downsample
+        # 检查是否为DTU数据集
+        self.is_dtu = "DTU" in datadir
 
         if downsample == 1:
             images_dir = os.path.join(datadir, "images")
@@ -85,6 +87,9 @@ class COLMAPDataset:
         self.poses = []
         self.all_rays = []
         self.all_rgbs = []
+        if self.is_dtu:
+            self.all_alphas = []  # 新增alpha通道存储
+
         for image in tqdm(self.images):
             c2w = torch.tensor(
                 image.cam_from_world.inverse().matrix(), dtype=torch.float32
@@ -100,16 +105,27 @@ class COLMAPDataset:
             world_rays = world_rays.reshape(self.img_wh[1], self.img_wh[0], 6)
 
             im = Image.open(os.path.join(images_dir, image.name))
-            im = im.convert("RGB")
-            rgbs = torch.tensor(np.array(im), dtype=torch.float32) / 255.0
+            # 如果是DTU数据集，则将图像转换为RGBA格式
+            if self.is_dtu:
+                im = im.convert("RGBA")
+                rgbas = torch.tensor(np.array(im), dtype=torch.float32) / 255.0
+                rgbs = rgbas[..., :3] * rgbas[..., 3:4] + (1 - rgbas[..., 3:4])  # white bg
+            else:
+                im = im.convert("RGB")
+                rgbs = torch.tensor(np.array(im), dtype=torch.float32) / 255.0
+
             im.close()
 
             self.all_rays.append(world_rays)
             self.all_rgbs.append(rgbs)
+            if self.is_dtu:
+                self.all_alphas.append(rgbas[..., -1:])  # 保存alpha通道
 
         self.poses = torch.stack(self.poses)
         self.all_rays = torch.stack(self.all_rays)
         self.all_rgbs = torch.stack(self.all_rgbs)
+        if self.is_dtu:
+            self.all_alphas = torch.stack(self.all_alphas)  # 堆叠alpha通道
 
         self.points3D = []
         self.points3D_color = []
